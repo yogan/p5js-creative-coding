bee5e6760d962996983473ab1b1647a8ac27600e
Frank Blendinger <yogan@zogan.de>
2025-10-05 12:45:34 +0200

    Add workflow to publish on GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
+	base: process.env.NODE_ENV === "production" ? "/p5js-creative-coding/" : "/",
 	server: {
 		open: true,
 		port: 3000,
