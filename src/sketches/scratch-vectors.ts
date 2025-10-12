bec2bd711e9fa3f58598461d5c95e6b72ada4588
Frank Blendinger <yogan@zogan.de>
2025-10-12 18:06:50 +0200

    3D bouncing ball in a box

--- src/sketches/scratch-vectors.ts
+++ src/sketches/scratch-vectors.ts
@@ -1,31 +1,82 @@
 import type p5 from "p5"
 
 export const scratchVectors = (p: p5) => {
-	const CIRCLE_SIZE = 50
-	let circle: p5.Vector
+	let min: p5.Vector
+	let max: p5.Vector
+	let ball: p5.Vector
+	let size: number // radius of the ball
 	let velocity: p5.Vector
 
 	p.setup = () => {
-		p.createCanvas(p.windowWidth, p.windowHeight)
+		p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
 
-		circle = p.createVector(p.random(p.width), p.random(p.height))
-		velocity = p.createVector(p.random(-5, 5), p.random(-5, 5))
+		// Lower and upper bounds define a box in which the circle can move
+		const depth = Math.min(p.width, p.height) / 2
+		min = p.createVector(-p.width / 4, -p.height / 4, 0)
+		max = p.createVector(p.width / 4, p.height / 4, depth)
+
+		// Place circle at random position within the box
+		size = Math.min(p.width, p.height) / 25
+		ball = p.createVector(
+			p.random(min.x + size, max.x - size),
+			p.random(min.y + size, max.y - size),
+			p.random(min.z + size, max.z - size),
+		)
+
+		// Random initial velocity, relative to ball size
+		const speedMin = size / 20
+		const speedMax = size / 10
+		velocity = p.createVector(
+			p.random([-1, 1]) * p.random(speedMin, speedMax),
+			p.random([-1, 1]) * p.random(speedMin, speedMax),
+			p.random([-1, 1]) * p.random(speedMin, speedMax),
+		)
+
+		// Initial camera position
+		const distance = Math.max(p.width, p.height)
+		// biome-ignore format: manually formatted
+		p.camera(
+			distance * 0.12,  // x: a bit to the left
+			-distance * 0.2,  // y: slightly from above
+			distance * 0.8,   // z: move back
+			0, 0, 0,          // look at center of box
+			0, 1, 0           // up vector
+		)
 	}
 
 	p.draw = () => {
 		p.background(220)
+		p.orbitControl()
 
-		p.fill(175)
-		p.stroke(0)
+		// Draw bounding box
+		p.push()
+		p.noFill()
+		p.translate((min.x + max.x) / 2, (min.y + max.y) / 2, (min.z + max.z) / 2)
+		p.box(max.x - min.x, max.y - min.y, max.z - min.z)
+		p.pop()
 
-		p.circle(circle.x, circle.y, CIRCLE_SIZE)
+		// Move ball
+		ball.add(velocity)
 
-		circle.add(velocity)
-
-		if (circle.x < CIRCLE_SIZE / 2 || circle.x > p.width - CIRCLE_SIZE / 2)
+		// Bounce off walls
+		if (ball.x <= min.x + size || ball.x >= max.x - size) {
 			velocity.x *= -1
-		if (circle.y < CIRCLE_SIZE / 2 || circle.y > p.height - CIRCLE_SIZE / 2)
+			ball.x = p.constrain(ball.x, min.x + size, max.x - size)
+		}
+		if (ball.y <= min.y + size || ball.y >= max.y - size) {
 			velocity.y *= -1
+			ball.y = p.constrain(ball.y, min.y + size, max.y - size)
+		}
+		if (ball.z <= min.z + size || ball.z >= max.z - size) {
+			velocity.z *= -1
+			ball.z = p.constrain(ball.z, min.z + size, max.z - size)
+		}
+
+		// Draw ball
+		p.push()
+		p.translate(ball.x, ball.y, ball.z)
+		p.sphere(size)
+		p.pop()
 	}
 
 	p.windowResized = () => {
