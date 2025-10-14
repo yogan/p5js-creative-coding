6fe21dec5559178bd4e3eec575f1db2f8c85d93a
Frank Blendinger <yogan@zogan.de>
2025-10-14 20:40:34 +0200

    Include E2E tests in CI/CD pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	workers: process.env.CI ? 1 : undefined,
 	reporter: "html",
 	use: {
-		baseURL: "http://127.0.0.1:4173",
+		baseURL: "http://localhost:9999",
 		trace: "on-first-retry",
 	},
 	projects: [
@@ -18,8 +18,8 @@ export default defineConfig({
 		},
 	],
 	webServer: {
-		command: "npm run build && npm run preview",
-		url: "http://127.0.0.1:4173",
+		command: "npm run build && npm run preview -- --port 9999",
+		url: "http://localhost:9999",
 		reuseExistingServer: !process.env.CI,
 	},
 })
